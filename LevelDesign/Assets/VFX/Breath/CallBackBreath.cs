98d0551bde9359e9d78d2944a22e709b10ed7d73
Unknown <skyesladeut@outlook.com>
2020-02-29 14:10:45 -0700

    Adding Building Textures and Some VFX

---
+++ LevelDesign/Assets/VFX/Breath/CallBackBreath.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallBackBreath : MonoBehaviour
+{
+    public bool running;
+    public float minwaittime, maxwaittime;
+
+    public IEnumerator OnParticleSystemStopped()
+    {
+        if (running)
+        {
+            yield return new WaitForSeconds(Random.Range(minwaittime, maxwaittime));
+            GetComponent<ParticleSystem>().Play();
+        }
+    }
+}
