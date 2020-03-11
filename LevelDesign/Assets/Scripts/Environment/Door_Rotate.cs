77e1497417a8b0aaa192a457c1e00432249a1d02
Skye <skyesladeut@outlook.com>
2020-03-11 14:12:14 -0600

    Scripts

---
+++ LevelDesign/Assets/Scripts/Environment/Door_Rotate.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Door_Rotate : MonoBehaviour
+{
+    public GameObject Hinge;
+    public float RotateAmount, Speed;
+    private bool open, inRange, running;
+    private float rotateAmount, totalRotateAmount, partialRotate;
+
+    private void Start()
+    {
+        open = false;
+        running = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inRange = true;
+            if (!running)
+            {
+                running = true;
+                StartCoroutine(openDoor());
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            inRange = false;
+        }
+    }
+
+    IEnumerator openDoor()
+    {
+        while (inRange)
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+                if (open)
+                {
+                    rotateAmount = RotateAmount;
+                    open = false;
+                }
+                else
+                {
+                    rotateAmount = -RotateAmount;
+                    open = true;
+                }
+
+                totalRotateAmount = 0;
+                while (totalRotateAmount >= rotateAmount+1f || totalRotateAmount <= rotateAmount - 1f)
+                {
+                    partialRotate = rotateAmount * Speed * Time.deltaTime;
+                    transform.RotateAround(Hinge.transform.position, Vector3.up, partialRotate);
+                    totalRotateAmount += partialRotate;
+                    yield return new WaitForFixedUpdate();
+                }
+            }
+            yield return new WaitForFixedUpdate();
+        }
+
+        running = false;
+    }
+}
---
+++ LevelDesign/Assets/Scripts/Environment/IncreaseCold.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class IncreaseCold : MonoBehaviour
+{
+    public float IncreaseAmount;
+    private float total;
+    public PostProcessVolume volume;
+    private Vignette vignette;
+
+    private void Start()
+    {
+        total = 0;
+        volume.profile.TryGetSettings(out vignette);
+    }
+
+    IEnumerator ColdChange()
+    {
+        yield return new WaitForFixedUpdate();
+    }
+    
+     
+}
